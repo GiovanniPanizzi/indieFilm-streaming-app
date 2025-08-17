27df1ff2d6032449baba3db75c4ef68acffe2caa
giovannipanizzi <giovannipanizzi5@gmail.com>
2025-08-17 14:21:32 +0200

    changed hero text in home page, added new class video__dark__overlay

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -7,19 +7,20 @@ function Main({ className }: { className?: string }) {
     return (
         <main className={className}>
             <div className="hero__content">
+                <div className="video__dark__overlay"></div>
                 <video src="/public/videos/ModernTimesNCProduction.mp4" className="hero__video__background" autoPlay loop muted>
                 </video>
                 <div className="title__and__explenation__text">
-                    <h1>Welcome to INDIE FILMS</h1>
-                    <p>Watch, share, and connect: indie films and film crew networking in one app</p>
+                    <h1>INDIE FILMS</h1>
+                    <p>Watch, share, connect</p>
                 </div>
             </div>
             <div className="main__content">
                 <h2>INDIE FILMS is a platform where you can: </h2>
                 <ul className="vision__list">
-                    <li className="vision__list__li">Whatch the best indipendent films</li>
+                    <li className="vision__list__li">Whatch the best indipendent films, shorts and series</li>
                     <li className="vision__list__li">Publish your masterpieces</li>
-                    <li className="vision__list__li">Make connection with creatives in the films, watch their contents and collaborate</li>
+                    <li className="vision__list__li">Make connection with creatives in the film industry, watch their contents and collaborate</li>
                 </ul>
             </div>
         </main>
