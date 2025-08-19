d563779de86cf38b417209eb3d995510d5914943
giovannipanizzi <giovannipanizzi5@gmail.com>
2025-08-19 12:01:34 +0200

    header is now responsive for most devices, added NOT functionable animated hamburger_nav_icon

--- client/src/components/header/NavBarMain.tsx
+++ client/src/components/header/NavBarMain.tsx
@@ -1,5 +1,6 @@
 //import components
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 //import styles
 import './NavBarMain.css';
@@ -11,16 +12,27 @@ type MenuItem = {
 };
 
 function NavBarMain({ className, items }: { className?: string, items: MenuItem[] }) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={className}>
       <input type="text" placeholder="Search films, actors, directors..." />
-      <ul>
+      <ul className="main__nav__bar__ul">
         {items.map((item, index) => (
             <li key={index}>
               <Link to={item.to} className={item.className} >{item.label}</Link>
             </li>
           ))}
       </ul>
+      <div className={`hamburger__nav__icon ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+          <span className="hamburger__nav__icon__line"></span>
+          <span className="hamburger__nav__icon__line"></span>
+      </div>
     </nav>
   );
 }
